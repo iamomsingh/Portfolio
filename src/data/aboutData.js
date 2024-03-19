5ccdb1c67c4f23fd9d2935dca31ec5d26737cb65
om singh <iamomsingh11@gmail.com>
2024-03-19 20:32:46 +0530

    changes in about data

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,5 +1,7 @@
 export const aboutData = {
-    title: "Who I am",
-    description1: "My name is OM SINGH. I'm a web designer and developer based in Bengaluru, INDIA.",
-    description2: "I am a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. Love almost all the stacks of Software Engineering. My current stack includes Nodejs, React, NextJs, Redux, Bootstrap, Ant Design, Material UI, MongoDB, Mysql, Firebase, Cloudinary, etc.",
-}
+  title: "Who I am",
+  description1:
+    "My name is OM SINGH. I'm a Full-stack web developer based in Bengaluru, INDIA.",
+  description2:
+    "I am a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and I  am Passionate about Problem Solving. Love almost all the stacks of Software Engineering. My current stack includes Nodejs, React, NextJs, Redux, Bootstrap, Ant Design, Material UI, MongoDB, Mysql, Firebase, Cloudinary, etc.",
+};
