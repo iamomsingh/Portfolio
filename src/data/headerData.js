a862a7f41e67de58abc0ae4762f1c43264dbb424
om singh <iamomsingh11@gmail.com>
2024-03-21 18:26:38 +0530

    changes before builds

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,8 @@
-
 export const headerData = {
-    name: 'OM SINGH',
-    title: "Hello! I am",
-    desciption: "Hello! I am OM SINGH, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
-    image: 'https://ibb.co/vzVG9Bx',
-    imagebw: 'https://i.ibb.co/Cw2Xp90/abu-said-bw.jpg',
-    resumePdf: 'https://orange-zorah-85.tiiny.site/'
-}
+  name: "OM SINGH",
+  title: "Hello! I am",
+  desciption:
+    "Hello! I am OM SINGH, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
+  image: "https://i.im.ge/2024/03/19/R3Yh8Y.Untitled.jpeg",
+  resumePdf: "https://orange-zorah-85.tiiny.site/",
+};
